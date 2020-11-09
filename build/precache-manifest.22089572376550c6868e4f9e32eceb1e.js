b7c209fe95c20cf5f0984f7f5d718d6907a03032
cally2k <callum.rafter@gmail.com>
2020-11-09 17:13:17 +0000

    V1

---
+++ build/precache-manifest.22089572376550c6868e4f9e32eceb1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2638d969e07b2986c07a5e53157a408f",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "8559930d3a1db6917275",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8d7fbd0be7e60c15ccda",
+    "url": "/face-recognition/static/css/main.61c4fa37.chunk.css"
+  },
+  {
+    "revision": "8559930d3a1db6917275",
+    "url": "/face-recognition/static/js/2.c6b80c64.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.c6b80c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7fbd0be7e60c15ccda",
+    "url": "/face-recognition/static/js/main.be8868f1.chunk.js"
+  },
+  {
+    "revision": "e08d123b2da8529cc1a5",
+    "url": "/face-recognition/static/js/runtime-main.20770ab4.js"
+  },
+  {
+    "revision": "987368d4cd98c7fcf341f1805b99cd07",
+    "url": "/face-recognition/static/media/cr.987368d4.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import Particles from "react-particles-js"
+import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Rank from "./components/Rank/Rank"
+import './App.css';
+
+const particlesOptions ={
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    }
+  }
+} 
+
+const initialState = {
+  input: "",
+  imageURL: "",
+  box: {},
+  route: "SignIn",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+      fetch("https://secret-woodland-58568.herokuapp.com/imageurl", {
+        method: "post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch("https://secret-woodland-58568.herokuapp.com/image", {
+            method: "put",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+          .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === "SignOut") {
+      this.setState(initialState)
+    } else if (route === "Home") {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const {isSignedIn, imageURL, route, box} = this.state;
+    return (
+      <div className="App">
+        <Particles className="particles"
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === "Home"
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageURL={imageURL}/>
+            </div>
+        
+          : (
+            route === "SignIn"
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
